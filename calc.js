3a6a0e9a12b881cfc5ebdf52f5063083896bce3a
atg205 <p.hanu@mailbox.org>
2020-10-06 17:21:41 -0400

    Finished calculation algorithm, made some minor design improvements, added signature

--- calc.js
+++ calc.js
@@ -1,10 +1,18 @@
-let firstNumber = "";
-let secondNumber = "";
-let values = [null, null, null];
-let display = document.getElementById('display');
+let result; 
+let input;
+let begin;
+let enterPressed;
 let operation;
-let firstOperation = true;
-let afterEnterPressed = false
+let display = document.getElementById('display');
+
+function init() {
+    result = "";
+    input = "";
+    begin = true;
+    enterPressed = false;
+}
+
+init();
 
 function addEventListenerToButtons() {
     let calculatorDiv = document.getElementById('calculator');
@@ -13,77 +21,87 @@ function addEventListenerToButtons() {
     })
 }
 
+addEventListenerToButtons();
+
 function buttonClicked(target) {
     let targetClass = target.classList[0];
-    if(targetClass == 'number') {
-        numberInput(target.innerHTML);
-        if(afterEnterPressed) {
-             firstNumber = '';
-             afterEnterPressed = false;
-        }
+    let targetFunction = window[target.value];
+
+    if(enterPressed) {
+        input = '';
         
-    }
-    
-    if(targetClass == 'operand') {
-        if(firstOperation) {
-            if(firstNumber == "") firstNumber = secondNumber;
-            firstOperation = false;
+        if(targetClass == 'number') {
+            init();
         }
-        else {
-            evaluate();
+        if(targetClass == 'operand') {
+            operation = targetFunction;
         }
-        secondNumber = '';
-        operation = window[target.value];
-        
     }
-    if(targetClass == 'functional') {
-        window[target.value]();
+   
+    switch (targetClass) {
+        case 'number':
+            numberInput(target.innerHTML);
+            break;
+        case 'operand':
+            if(begin) {
+                result = input;
+                begin = false;
+                operation = targetFunction;
+                enterPressed = false;
+            }
+            else if (!enterPressed){
+                result = operation(+result, +input);
+                operation = targetFunction;
+            }
+            updateDisplay(result);
+            input = '';
+            enterPressed = false; 
+            break;
+        case 'functional':
+            targetFunction();
+            input = "";
+            break;
+        default:
+            console.log(target);
     }
 }
+
 
 function enter() {
-    afterEnterPressed = true;
-    evaluate();
-}
-
-function evaluate() {
-    firstNumber = operation(+firstNumber, +secondNumber);
-    updateDisplay(firstNumber);
-    firstOperation = true;
-    secondNumber = '';
+    enterPressed = true;
+    result = operation(+result, +input);
+    updateDisplay(result);
 }
 
 function clear() {
-    firstNumber = "", secondNumber = "";
+    begin = true;
+    init();
     updateDisplay(0);
-    firstOperation = true;
 }
 
-function updateDisplay(message = secondNumber) {
+function updateDisplay(message = result) {
     display.innerHTML = message;
 }
 
-addEventListenerToButtons();
 
 function add(a = 0, b = 0){
-    console.log(`adding ${a} and ${b}`);
     return a + b
 }
 
-function substract(a, b) {
+function substract(a, b = 0) {
     return a - b
 }
 
-function multiply(a, b) {
+function multiply(a, b = 1) {
     return a * b
 }
 
-function divide(a , b){
+function divide(a , b = 1){
     return a / b
 }
 
 function numberInput(number) {
-    values[0] += number;
-    updateDisplay(values[0]);
+    input += number;
+    updateDisplay(input);
 }
 
